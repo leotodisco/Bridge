bba296753dfecc6e652f07531338b451f127fc5b
Leopoldo Todisco <80098232+leotodisco@users.noreply.github.com>
2024-12-01 17:49:48 +0100

    test CI

--- bridge-backend/src/main/java/com/project/bridgebackend/BridgeBackendApplication.java
+++ bridge-backend/src/main/java/com/project/bridgebackend/BridgeBackendApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BridgeBackendApplication {
 
     public static void main(String[] args) {
-        System.out.println("ciao");
+        System.out.println("ciao!!!!!!");
         SpringApplication.run(BridgeBackendApplication.class, args);
     }
 
