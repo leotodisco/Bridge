5d17a1e3de57b75117f749b2707cd5272ef00a80
GuardianMont <gerldinemontella2@gmail.com>
2024-12-03 19:47:18 +0100

    add (backend) : Insert entity Annuncio && Consulenza
    inserite le entity di annuncio e l'entità di consulenza, le due entità sono legate da un rapporto di ereditarietà

---
+++ bridge-backend/src/main/java/com/project/bridgebackend/Model/Entity/Consulenza.java
@@ -0,0 +1,49 @@
+package com.project.bridgebackend.Model.Entity;
+
+import com.project.bridgebackend.Model.Entity.enumeration.TipoConsulenza;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "consulenza")
+public class Consulenza extends Annuncio {
+
+    /*
+    * Tipo identifica la tipologia di consulenza
+    * che si vuole proporre
+    * */
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tipo", nullable = false)
+    private TipoConsulenza tipo;
+
+    /*
+    * Orari di disponibilità indicano la fascia oraria
+    * in cui il consulente è disponbile
+    * todo: to verify if string is a valid option
+     */
+    @Column(name = "orari_disponibili", nullable = false, length = 500)
+    private String orariDisponibili;
+
+    /*
+    * descrizione è una stringa atta a discriminare come la
+    * consulenza mira ad aiutare il rifugiato
+    *
+    *
+    */
+    @NotBlank(message = "la descrizione non può essere vuota")
+    @Column(name = "descrizione", nullable = false, length = 1000)
+    private String descrizione;
+
+    @NotBlank(message = "Il numero non può essere vuoto")
+    @Pattern(
+            regexp = "^((00|\\+)39[\\. ]??)??3\\d{2}[\\. ]??\\d{6,7}$",
+            message = "Numero di telefono non valido. Deve seguire il formato italiano."
+    )
+    @Column(name = "numero", nullable = false, length = 15)    private String numero;
+}
---
+++ bridge-backend/src/main/java/com/project/bridgebackend/Model/Entity/enumeration/TipoConsulenza.java
@@ -0,0 +1,9 @@
+package com.project.bridgebackend.Model.Entity.enumeration;
+
+public enum TipoConsulenza {
+    SANITARIA,
+    LEGALE,
+    COMMERCIALE,
+    PSICOLOGICA,
+    TRADUTTORE
+}
