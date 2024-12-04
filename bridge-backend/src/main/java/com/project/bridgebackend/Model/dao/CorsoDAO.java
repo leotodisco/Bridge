2b7e841dd85921962bd3620ef77db37be1ec3b4f
Biagio Gallo <b.gallo9@studenti.unisa.it>
2024-12-04 17:40:13 +0100

    Corso & CorsoDAO fix
    sono stati aggiunti metodi al file CorsoDAO e sono stati modificati gli attributi dell'Entity Corso

--- bridge-backend/src/main/java/com/project/bridgebackend/Model/dao/CorsoDAO.java
+++ bridge-backend/src/main/java/com/project/bridgebackend/Model/dao/CorsoDAO.java
@@ -1,8 +1,12 @@
 package com.project.bridgebackend.Model.dao;
 
 import com.project.bridgebackend.Model.Entity.Corso;
+import com.project.bridgebackend.Model.Entity.Volontario;
+import com.project.bridgebackend.Model.Entity.enumeration.Lingua;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 /**
@@ -14,10 +18,26 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CorsoDAO extends JpaRepository<Corso, Long> {
+
     /**
-     * Questo metodo permette di trovare un corso tramite il suo id.
-     * @param id id del corso da trovare.
+     * Questo metodo permette di trovare un corso tramite il suo proprietario
+     * @param proprietario proprietario del corso
      * @return il corso trovato.
      */
-    Corso findById(long id);
+    List<Corso> findByProprietario(Volontario proprietario);
+
+    /**
+     * Questo metodo permette di trovare un corso tramite il genere
+     * @param categoriaCorso genere del corso
+     * @return il corso trovato.
+     */
+    Corso findByCategoriaCorso(String categoriaCorso);
+
+    /**
+     * Questo metodo permette di trovare un corso tramite la lingua
+     * @param lingua lingua del corso
+     * @return il corso trovato.
+     */
+    Corso findByLingua(Lingua lingua);
+
 }
