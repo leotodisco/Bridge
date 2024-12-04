dd2154a7d02e3917364799ca7058dfe10f8fd303
Alessia De Filippo <alessiadefilippo93@gmail.com>
2024-12-04 14:31:08 +0100

    add(backend): creazione DAO "Evento"

--- bridge-backend/src/main/java/com/project/bridgebackend/Model/dao/EventoDAO.java
+++ bridge-backend/src/main/java/com/project/bridgebackend/Model/dao/EventoDAO.java
@@ -1,16 +1,61 @@
 package com.project.bridgebackend.Model.dao;
 
+import com.project.bridgebackend.Model.Entity.Evento;
 import com.project.bridgebackend.Model.Entity.Indirizzo;
+import com.project.bridgebackend.Model.Entity.Volontario;
+import com.project.bridgebackend.Model.Entity.enumeration.Lingua;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 /**
  * @author Alessia De Filippo.
- * creato il 03/12/2024.
+ * Creato il 03/12/2024.
  * DAO della classe Evento.
  */
 
 @Repository
-public interface EventoDAO extends JpaRepository<Indirizzo, Long> {
+public interface EventoDAO extends JpaRepository<Evento, Long> {
 
+    /**
+     * Trova eventi per lingua parlata.
+     * @param lingua lingua dell'evento
+     * @return lista di eventi
+     */
+    List<Evento> findByLingueParlate(Lingua lingua);
+
+
+    /**
+     * Restituisce gli eventi svolti in determinata data
+     * @param data Data dell'evento
+     * @return Lista di eventi
+     */
+    List<Evento> findByData(LocalDate data);
+
+    /**
+     * Restituisce gli eventi che si svolgono in un determinato luogo
+     * @param luogo Luogo dell'evento
+     * @return Lista di eventi
+     */
+    List<Evento> findByLuogo(Indirizzo luogo);
+
+    /**
+     * Restituisce gli eventi organizzati da un determinato volontario.
+     * @param organizzatore Volontario organizzatore dell'evento
+     * @return Lista di eventi
+     */
+    List<Evento> findByOrganizzatore(Volontario organizzatore);
+
+    /**
+     * Elimina gli eventi con data precedente alla data fornita.
+     * @param dataBefore Data di riferimento per l'eliminazione
+     */
+    @Transactional
+    void deleteByDataBefore(LocalDate dataBefore);
 }
+
+
+
