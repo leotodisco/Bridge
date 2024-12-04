a56f00de283e691518fe2b7bcecbfb8851d89e0e
Vito Vernellati <v.vernellati@studenti.unisa.it>
2024-12-04 10:24:03 +0100

    add(bridge-backend): Aggiunti Lavoro Entity e TipoContratto

---
+++ bridge-backend/src/main/java/com/project/bridgebackend/Model/Entity/Lavoro.java
@@ -0,0 +1,96 @@
+package com.project.bridgebackend.Model.Entity;
+
+/**
+ * @author Vito Vernellati
+ * @created 04/12/2024
+ * @version 1.0
+ * Entità per la gestione degli annunci di lavoro
+ */
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import javax.validation.constraints.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "Lavoro")
+public class Lavoro {
+
+    /**
+     * ID incrementale (Genera automaticamente il valore della chiave primaria)
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    /**
+     * Posizione Lavorativa
+     */
+    @Column(nullable = false, length = 100)
+    @NotBlank(message = "La posizione lavorativa è obbligatoria")
+    @Size(max = 100, message = "La posizione lavorativa non può superare i 100 caratteri")
+    private String posizioneLavorativa;
+
+    /**
+     * Nome Azienda
+     */
+    @Column(nullable = false, length = 100)
+    @NotBlank(message = "Il nome dell'azienda è obbligatorio")
+    @Size(max = 100, message = "Il nome dell'azienda non può superare i 100 caratteri")
+    private String nomeAzienda;
+
+    /**
+     * Orario di Lavoro
+     */
+    @Column(nullable = false, length = 50)
+    @NotBlank(message = "L'orario di lavoro è obbligatorio")
+    @Size(max = 50, message = "L'orario di lavoro non può superare i 50 caratteri")
+    private String orarioLavoro;
+
+    /**
+     * Tipo di Contratto
+     */
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 50)
+    @NotNull(message = "Il tipo di contratto è obbligatorio")
+    private TipoContratto tipoContratto;
+
+    /**
+     * Retribuzione
+     */
+    @Column(nullable = false, precision = 10, scale = 2)
+    @DecimalMin(value = "0.0", inclusive = false, message = "La retribuzione deve essere positiva")
+    private double retribuzione;
+
+    /**
+     * Nome Sede
+     */
+    @Column(nullable = false, length = 100)
+    @NotBlank(message = "Il nome della sede è obbligatorio")
+    @Size(max = 100, message = "Il nome della sede non può superare i 100 caratteri")
+    private String nomeSede;
+
+    /**
+     * Info Utili
+     */
+    @Column(nullable = false, length = 500)
+    @NotBlank(message = "Le info utili sono obbligatorie")
+    @Size(max = 500, message = "Le info utili non possono superare i 500 caratteri")
+    private String infoUtili;
+
+    /**
+     * Indirizzo
+     */
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "indirizzo_id", nullable = false)
+    @NotNull(message = "L'indirizzo è obbligatorio")
+    private Indirizzo indirizzo;
+
+    /**
+     * Costruttore
+     */
+    public Lavoro () {}
+}
---
+++ bridge-backend/src/main/java/com/project/bridgebackend/Model/Entity/enumeration/TipoContratto.java
@@ -0,0 +1,12 @@
+package com.project.bridgebackend.Model.Entity.enumeration;
+
+public enum TipoContratto {
+    FULL_TIME,
+    PART_TIME,
+    APPRENDISTATO,
+    INTERNSHIP,
+    PROGETTO,
+    COLLABORAZIONE,
+    COOPERATIVA,
+    ALTRO
+}
