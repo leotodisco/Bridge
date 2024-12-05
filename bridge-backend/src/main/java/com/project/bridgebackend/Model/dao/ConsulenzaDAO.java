dda08ee7f7b4244ee4b9146a78d97eb2f34848b5
GuardianMont <gerldinemontella2@gmail.com>
2024-12-05 19:55:19 +0100

    FIX(backend) : commenti
    Aggiunti commenti e check
    
    Signed-off-by: GuardianMont <gerldinemontella2@gmail.com>

--- bridge-backend/src/main/java/com/project/bridgebackend/Model/dao/ConsulenzaDAO.java
+++ bridge-backend/src/main/java/com/project/bridgebackend/Model/dao/ConsulenzaDAO.java
@@ -2,6 +2,13 @@ package com.project.bridgebackend.Model.dao;
 
 import org.springframework.stereotype.Repository;
 
+/*
+ * @author Geraldine Montella.
+ * Creato il: 03/12/2024.
+ * Questa Java Persistence Entity per un annuncio,
+ * la inheritance è settata a JOINED perciò Ogni entità ha la propria tabella,
+ * e le tabelle sono unite tramite chiavi primarie.
+ */
 @Repository
 public interface ConsulenzaDAO extends AnnuncioDAO{
 
